2da39cd42eef6287879e3efca08a09b4311b602d
Ebrahimi86 <109447867+Ebrahimi86@users.noreply.github.com>
2022-08-25 07:26:54 -0700

    Add files via upload

---
+++ Paying workers.py
@@ -0,0 +1,6 @@
+print("Hello dear worker,")
+X = input("How many hours did you work today:")
+X = float(X)
+print("Each working hour is 200 IRT")
+print("Your salary is >>" , X*200 , "IRT << for today " )
+print("Good job!")
